a870bab7c0b9ccb4c150dc2dd8f1df8c62523382
GrassyKudzu <138802581+GrassyKudzu@users.noreply.github.com>
2023-07-18 15:19:45 -0500

    Update defang_v4.ps1
    v4 redux

--- defang_v4.ps1
+++ defang_v4.ps1
@@ -1,20 +1,19 @@
-param(
-    [string]$url = ""
-)
-
 function Defang-Url {
     param(
         [string]$url
     )
 
-    $defangedUrl = $url -replace ":", "x" -replace "\.", "[.]"
-    return "h" + $defangedUrl
+    # Replace ':' with 'x' and '.' with '[.]'
+    $defangedUrl = $url -replace ':', 'x' -replace '\.', '[.]'
+    return "h$defangedUrl"
 }
 
 function Get-InputUrl {
-    if ($url -ne "") {
-        return $url.Trim()
-    } else {
+    param(
+        [string]$url
+    )
+
+    if (-not $url) {
         $clipboardContent = Get-Clipboard
         if ($clipboardContent) {
             return $clipboardContent.Trim()
@@ -23,8 +22,34 @@ function Get-InputUrl {
             exit 1
         }
     }
+
+    return $url.Trim()
 }
 
-$normalUrl = Get-InputUrl
+function Test-DefangUrl {
+    param(
+        [string]$inputUrl,
+        [string]$expectedResult
+    )
+
+    $defangedUrl = Defang-Url -url $inputUrl
+    $isEqual = $defangedUrl -eq $expectedResult
+
+    Write-Host "Testing Defang-Url:"
+    Write-Host "Input URL: $inputUrl"
+    Write-Host "Defanged URL: $defangedUrl"
+    Write-Host "Expected Result: $expectedResult"
+    Write-Host "Test Result: $($isEqual -as [string])"
+    Write-Host ""
+}
+
+# Test cases
+Test-DefangUrl -inputUrl "https://www.example.com" -expectedResult "hxxps://www[.]example[.]com"
+Test-DefangUrl -inputUrl "http://example.org/something?param=value" -expectedResult "hxxp://example[.]org/something?param=value"
+Test-DefangUrl -inputUrl "ftp://ftp.example.com" -expectedResult "hftp://ftp[.]example[.]com"
+Test-DefangUrl -inputUrl "file://shared/files/document.txt" -expectedResult "hfile://shared/files/document[.]txt"
+
+# Get the URL from the command-line argument or clipboard
+$normalUrl = Get-InputUrl -url $args[0]
 $defangedUrl = Defang-Url -url $normalUrl
 Write-Host "DEFANGED URL: $defangedUrl"
