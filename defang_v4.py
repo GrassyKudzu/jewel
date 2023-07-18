ec95a420cacbdd2d2ca18707c1c7c8f1ce89cd51
GrassyKudzu <138802581+GrassyKudzu@users.noreply.github.com>
2023-07-18 16:02:05 -0500

    Update defang_v4.py
    Add modify_protocol

--- defang_v4.py
+++ defang_v4.py
@@ -11,11 +11,24 @@ def defang_url(url):
 
     return url
 
+def modify_protocol(url):
+    if url.startswith("http://"):
+        url = url.replace("http://", "hxxp://", 1)
+    elif url.startswith("https://"):
+        url = url.replace("https://", "hxxps://", 1)
+    elif url.startswith("ftp://"):
+        url = url.replace("ftp://", "fxp://", 1)
+    elif url.startswith("file://"):
+        url = url.replace("file://", "fxxe://", 1)
+
+    return url
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <URL>")
         sys.exit(1)
 
     normal_url = sys.argv[1]
-    defanged_url = defang_url(normal_url)
-    print("DEFANGED URL:", defanged_url)
+    safe_url = modify_protocol(normal_url)
+    defanged_url = defang_url(safe_url)
+    print("SAFE DEFANGED URL:", defanged_url)
