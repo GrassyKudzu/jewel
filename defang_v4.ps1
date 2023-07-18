19983ba8b67e97c4a8fb0733c71f95e4eaf8d839
GrassyKudzu <138802581+GrassyKudzu@users.noreply.github.com>
2023-07-18 15:56:01 -0500

    Update defang_v4.ps1
    Simplify

--- defang_v4.ps1
+++ defang_v4.ps1
@@ -3,53 +3,24 @@ function Defang-Url {
         [string]$url
     )
 
-    # Replace ':' with 'x' and '.' with '[.]'
-    $defangedUrl = $url -replace ':', 'x' -replace '\.', '[.]'
-    return "h$defangedUrl"
-}
+    $firstPeriodIndex = $url.IndexOf('.')
+    if ($firstPeriodIndex -ne -1) {
+        $url = $url.Substring(0, $firstPeriodIndex) + '[.' + $url.Substring($firstPeriodIndex + 1)
 
-function Get-InputUrl {
-    param(
-        [string]$url
-    )
-
-    if (-not $url) {
-        $clipboardContent = Get-Clipboard
-        if ($clipboardContent) {
-            return $clipboardContent.Trim()
-        } else {
-            Write-Host "No URL provided, and the clipboard is empty."
-            exit 1
+        $secondPeriodIndex = $url.IndexOf('.', $firstPeriodIndex + 2)
+        if ($secondPeriodIndex -ne -1) {
+            $url = $url.Substring(0, $secondPeriodIndex) + '.]' + $url.Substring($secondPeriodIndex + 1)
         }
     }
 
-    return $url.Trim()
+    return $url
 }
 
-function Test-DefangUrl {
-    param(
-        [string]$inputUrl,
-        [string]$expectedResult
-    )
-
-    $defangedUrl = Defang-Url -url $inputUrl
-    $isEqual = $defangedUrl -eq $expectedResult
-
-    Write-Host "Testing Defang-Url:"
-    Write-Host "Input URL: $inputUrl"
-    Write-Host "Defanged URL: $defangedUrl"
-    Write-Host "Expected Result: $expectedResult"
-    Write-Host "Test Result: $($isEqual -as [string])"
-    Write-Host ""
+if ($args.Length -ne 1) {
+    Write-Host "Usage: powershell script.ps1 <URL>"
+    exit 1
 }
 
-# Test cases
-Test-DefangUrl -inputUrl "https://www.example.com" -expectedResult "hxxps://www[.]example[.]com"
-Test-DefangUrl -inputUrl "http://example.org/something?param=value" -expectedResult "hxxp://example[.]org/something?param=value"
-Test-DefangUrl -inputUrl "ftp://ftp.example.com" -expectedResult "hftp://ftp[.]example[.]com"
-Test-DefangUrl -inputUrl "file://shared/files/document.txt" -expectedResult "hfile://shared/files/document[.]txt"
-
-# Get the URL from the command-line argument or clipboard
-$normalUrl = Get-InputUrl -url $args[0]
+$normalUrl = $args[0]
 $defangedUrl = Defang-Url -url $normalUrl
 Write-Host "DEFANGED URL: $defangedUrl"
